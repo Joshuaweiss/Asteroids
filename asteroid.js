6ae54b6cb74b0e18843cfce98241eec582f5ed91
joshua weiss <joshuaweiss@mac.com>
2015-06-29 11:57:52 -0400

    Create Random vector generator

--- asteroid.js
+++ asteroid.js
@@ -1,8 +1,19 @@
 (
-  var Asteroid = window.Asteroids.Asteroid = function() {};
+  var Coordinate = window.Asteroids.utils.Coordinate
+
+  var COLOR = "#AAAAAA";
+  var RADIUS = 50;
+
+  var Asteroid = window.Asteroids.Asteroid = function(options) {
+    this.color = options.color || COLOR;
+    this.radius = options.radius || RADIUS;
+    this.vel = options.vel || Coordinate.random();
+    this.pos = options.pos || Coordinate.random();
+
+  };
 
   Asteroid.inherits(window.Asteroids.MovingObject);
 
-  
+
 
 )();
--- utils.js
+++ utils.js
@@ -1,27 +1,33 @@
 (var Coordinate = window.Asteroids.utils.Coordinate = function(x, y) {
   this.x = x;
   this.y = y;
-}
+};
 
 
 Coordinate.prototype.dist = function(otherCoord){
   return Math.sqrt(Math.pow((this.x - otherCoord.x), 2)  + Math.pow((this.y - otherCoord.y), 2));
-}
+};
 
 Coordinate.prototype.norm = function(){
   return this.dist(new Coordinate(0,0));
-}
+};
 
 Coordinate.prototype.add = function(otherCoord){
   return new Coordinate(this.x + otherCoord.x, this.y + otherCoord.y);
-})();
+};
+
+Coordinate.prototype.random = function(){
+  return new Coorindate( Math.floor(1000*Math.random() ), Math.floor(1000*Math.random()) );
+};
+)();
 
 
-Function.prototype.inherits = function(parentClass) {
+(Function.prototype.inherits = function(parentClass) {
    var Surrogate = function() {};
    Surrogate.prototype = parentClass.prototype;
    this.prototype = new Surrogate();
-}
+};
+)();
 
 
 //
